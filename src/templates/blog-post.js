bb55fbcfdc23936f1e05b75a9915aee4d9e2d266
fallenhh <fallen2hh@gmail.com>
2020-09-19 17:30:14 +0800

    add SEO for post page

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 import "../styles/blogpost.sass"
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
   return (
     <Layout>
+      <SEO title={post.frontmatter.title}/>
       <div className="post-container">
         <div className="post-title">{post.frontmatter.title}</div>
         <div className="post-date">{post.frontmatter.date}</div>
